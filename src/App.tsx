23077d2093172053b34f98bc451b7a6b7cb282f3
ASproson <a.sproson@outlook.com>
2024-08-04 17:59:30 +0100

    Blocking

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-
 function App() {
-
   return (
-    <div className="h-screen bg-black">
-      <h1 className="text-white">Hello world</h1>
+    <div className="h-screen flex justify-center items-center bg-black text-white">
+      <div className="max-w-6xl border-white border-2 rounded-lg min-w-[650px]">
+        <div className="flex justify-between">
+          <div>enemies</div>
+          <div>characters</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
