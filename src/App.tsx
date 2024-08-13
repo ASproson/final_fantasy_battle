16db30e9982489c92e6bf458bdfdf9505ac58298
ASproson <a.sproson@outlook.com>
2024-08-13 15:08:01 +0100

    Renders player characters

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,31 @@ function App() {
     return <CharacterSheet key={name} name={name} hp={hp} />;
   });
 
+  const characterSprites = characterList.map(({ name, img }) => {
+    return <Character key={name} name={name} src={img} />;
+  });
+
   return (
     <div className="h-screen flex justify-center items-center bg-black text-white p-4">
       <div className="max-w-6xl border-white border-2 rounded-lg min-w-[570px] w-full h-full max-h-[700px] ml-2 flex">
         <div className="flex flex-col w-full">
-          <div className="h-[100px] border-2 border-red-500">background</div>
+          <div
+            className="h-[100px]"
+            style={{
+              backgroundImage: 'url(src/assets/battle-bg.png)',
+              backgroundSize: '100% auto',
+              backgroundPosition: 'right 0px bottom 200px',
+              backgroundAttachment: 'local',
+            }}
+          ></div>
           <div className="flex justify-between py-2 border-2 border-blue-500 min-h-[170px] h-3/5">
             <div className="flex flex-col justify-center">
               <div>enemy1</div>
               <div>enemy2</div>
               <div>enemy3</div>
             </div>
-            <div className="flex flex-col justify-center">
-              <div>char1</div>
-              <div>char2</div>
-              <div>char3</div>
-              <div>char4</div>
+            <div className="flex flex-col justify-center space-y-3 border-2 border-red-500 pr-10">
+              {characterSprites}
             </div>
           </div>
           <div className="flex flex-1 justify-between border-2 border-red-500 ">
@@ -42,3 +51,16 @@ function App() {
 }
 
 export default App;
+
+interface CharacterProps {
+  src: string;
+  name: string;
+}
+
+export const Character = ({ src, name }: CharacterProps) => {
+  return (
+    <div>
+      <img src={src} className="w-20" alt={name} />
+    </div>
+  );
+};
--- src/library/Characters.ts
+++ src/library/Characters.ts
@@ -16,8 +16,20 @@ class CharacterGenerator implements CharacterClass {
 }
 
 export const characterList = [
-  new CharacterGenerator('AAAA', 30, '/images/AAAA.png', 10, 0.2),
-  new CharacterGenerator('BBBB', 25, '/images/BBBB.png', 12, 0.3),
-  new CharacterGenerator('CCCC', 28, '/images/CCCC.png', 8, 0.25),
-  new CharacterGenerator('DDDD', 33, '/images/DDDD.png', 15, 0.15),
+  new CharacterGenerator('AAAA', 30, '/src/assets/bl-belt.png', 10, 0.2),
+  new CharacterGenerator('BBBB', 25, '/src/assets/bl-mage.png', 12, 0.3),
+  new CharacterGenerator(
+    'CCCC',
+    28,
+    '/src/assets/wh-mage-cleaned.png',
+    8,
+    0.25
+  ),
+  new CharacterGenerator(
+    'DDDD',
+    33,
+    '/src/assets/fighter-cleaned.png',
+    15,
+    0.15
+  ),
 ];
