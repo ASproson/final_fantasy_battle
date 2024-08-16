29ee8f702978d1c1f86b5c16c2facc82b14ab26c
ASproson <a.sproson@outlook.com>
2024-08-16 10:12:33 +0100

    Adds hovers to buttons

--- src/components/PlayerMenu.tsx
+++ src/components/PlayerMenu.tsx
@@ -10,7 +10,9 @@ export const PlayerMenu = () => {
   return (
     <div className="flex flex-col items-start flex-wrap font-bold text-lg p-4 h-full">
       {BUTTONS.map(({ title }) => (
-        <button key={title}>{title}</button>
+        <button key={title} className="text-slate-400 hover:text-slate-100 transition ease-in-out">
+          {title}
+        </button>
       ))}
     </div>
   );
