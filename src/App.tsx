5b95f905684f38ac77a84d13fe86ad3efc136a0f
ASproson <a.sproson@outlook.com>
2024-08-26 10:22:01 +0100

    Adds first menu modal render

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import { CharacterSheet } from './components/CharacterSheet';
 import { PlayerMenu } from './components/PlayerMenu';
 import { characterList, enemyList } from './library/Characters';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
   const characterSheetList = characterList.map(({ name, hp }) => {
     return <CharacterSheet key={name} name={name} hp={hp} />;
   });
@@ -17,7 +20,7 @@ function App() {
 
   return (
     <div className="h-screen flex justify-center items-center bg-black text-white p-4 uppercase">
-      <div className="max-w-6xl border-white border-2 rounded-lg w-full h-full max-h-[600px] ml-2 flex overflow-auto">
+      <div className="max-w-6xl border-white border-2 rounded-lg w-full h-full max-h-[600px] ml-2 flex overflow-auto relative">
         <div className="flex flex-col w-full">
           <div
             className="hidden sm:block sm:h-[100px] md:h-[150px] bg-contain"
@@ -30,13 +33,14 @@ function App() {
             <SpriteRenderer>{characterSprites}</SpriteRenderer>
           </div>
           <div className="border-2 border-red-500">
-            <PlayerMenu />
+            <PlayerMenu showModal={showModal} setShowModal={setShowModal} />
           </div>
         </div>
 
         <div className="flex items-end h-full">
           <div className="h-full w-[75px] p-1 pt-2">{characterSheetList}</div>
         </div>
+        {showModal && <MenuModal />}
       </div>
     </div>
   );
@@ -62,4 +66,8 @@ interface SpriteRendererProps {
 }
 export const SpriteRenderer = ({ children }: SpriteRendererProps) => {
   return <div className="flex flex-col flex-wrap justify-center space-y-3 px-10">{children}</div>;
+};
+
+export const MenuModal = () => {
+  return <div className="bg-white h-10 w-10 absolute top-0 left-0"></div>;
 };
--- src/components/PlayerMenu.tsx
+++ src/components/PlayerMenu.tsx
@@ -1,3 +1,5 @@
+import { Dispatch, SetStateAction } from 'react';
+
 const BUTTONS = [
   { title: 'FIGHT' },
   { title: 'MAGIC' },
@@ -6,19 +8,30 @@ const BUTTONS = [
   { title: 'RUN' },
 ];
 
+interface PlayerMenuProps {
+  showModal: boolean;
+  setShowModal: Dispatch<SetStateAction<boolean>>;
+}
+
 /**
  *
  * @returns Menu buttons
  * @todo onClicks pull button specific modal with actions to take
  */
-export const PlayerMenu = () => {
+export const PlayerMenu = ({ showModal, setShowModal }: PlayerMenuProps) => {
   return (
-    <div className="flex justify-around font-bold space-x-6 text-lg p-4 h-full">
-      {BUTTONS.map(({ title }) => (
-        <button key={title} className="text-slate-400 hover:text-slate-100 transition ease-in-out">
-          {title}
-        </button>
-      ))}
-    </div>
+    <>
+      <div className="flex justify-around font-bold space-x-6 text-lg p-4 h-full relative">
+        {BUTTONS.map(({ title }) => (
+          <button
+            key={title}
+            onClick={() => setShowModal(!showModal)}
+            className="text-slate-400 hover:text-slate-100 transition ease-in-out"
+          >
+            {title}
+          </button>
+        ))}
+      </div>
+    </>
   );
 };
